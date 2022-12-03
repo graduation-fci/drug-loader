742c8a95ec23a9ccff48eb63b795ce8c2c413ec8
0xSherlokMo <mohamed.sorour@halan.com>
2022-12-03 21:27:59 +0200

    building relations functionality

--- repository/graph.go
+++ repository/graph.go
@@ -46,4 +46,30 @@ func (d DrugRepository) InsetNode(name string) {
 }
 
 func (d DrugRepository) BuildRelations(drug *model.Drug) {
+	ctx := context.TODO()
+	session := d.graph.NewSession(ctx, neo4j.SessionConfig{})
+	defer session.Close(ctx)
+	for i, interaction := range drug.Interactions {
+		_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+			records, err := tx.Run(ctx, "MATCH (a:Drug {name: $src}) MATCH(b:Drug {name: $dest}) CREATE (a)-[rel:INTERACTS {effect: $effect, level: $level}]->(b) RETURN a.name", map[string]any{
+				"src":    drug.Name,
+				"dest":   interaction.Name,
+				"effect": interaction.Effect,
+				"level":  interaction.Level,
+			})
+			if err != nil {
+				return nil, err
+			}
+			record, err := records.Single(ctx)
+			if err != nil {
+				return nil, err
+			}
+			return &model.Drug{
+				Name: record.Values[0].(string),
+			}, nil
+		})
+		if err != nil {
+			log.Printf("Error while building relation %d ; err: %s \n", i, err)
+		}
+	}
 }
