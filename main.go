05763f580e7a211b01bd288f02005be81a596f24
0xSherlokMo <mohamed.sorour@halan.com>
2022-12-03 20:41:17 +0200

    starting point of app

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/graduation-fci/phase1-demo/dependencies"
+	"github.com/graduation-fci/phase1-demo/loader"
+)
+
+func main() {
+	dp := dependencies.NewDependencyInjection().WithNeo4j()
+	defer dp.Shutdown()
+
+	pipeline := loader.NewPipeline(dp)
+
+	pipeline.LoadNodes()
+	pipeline.LoadEdges()
+}
